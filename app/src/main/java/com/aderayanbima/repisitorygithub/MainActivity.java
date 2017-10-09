7db9077b97a83dcde139d8ce6000d0aa2ef61deb
aderayanbima31 <aderayanbima31@gmail.com>
2017-10-09 16:02:58 +0700

    nambahin tittle hello bima

--- app/src/main/java/com/aderayanbima/repisitorygithub/MainActivity.java
+++ app/src/main/java/com/aderayanbima/repisitorygithub/MainActivity.java
@@ -2,9 +2,13 @@ package com.aderayanbima.repisitorygithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView teks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
